b5968ac1b06d460ad7734daf2934cd8e771772eb
apostolV450 <a1826273@student.adelaide.edu.au>
2025-06-20 07:46:37 +0000

    [automatic commit] 2025-06-20 07:46:36 UTC 1.122.16.112

--- part1/part1.js
+++ part1/part1.js
@@ -10,7 +10,7 @@ const db = mysql.createPool({
 });
 
 // Helper function for DB queries
-function queryDB(s  ql, params = []) {
+function queryDB(sql, params = []) {
   return new Promise((resolve, reject) => {
     db.query(sql, params, (err, results) => {
       if (err) return reject(err);
