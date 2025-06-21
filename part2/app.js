efc30aef38bf91cf7435442e82293118954a69fa
apostolV450 <a1826273@student.adelaide.edu.au>
2025-06-21 03:15:01 +0000

    [automatic commit] 2025-06-21 03:15:01 UTC 1.122.16.112

--- part2/app.js
+++ part2/app.js
@@ -28,6 +28,11 @@ const userRoutes = require('./routes/userRoutes');
 
 app.use('/api/walks', walkRoutes);
 app.use('/api/users', userRoutes);
-
+///////////////////////
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Server running on http://localhost:${PORT}`);
+});
+/////////////////////////////////////////
 // Export the app instead of listening here
 module.exports = app;
