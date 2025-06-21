6c0cfb67614828970cb9e2c55f76bc9f3d91c946
apostolV450 <a1826273@student.adelaide.edu.au>
2025-06-21 02:35:57 +0000

    [automatic commit] 2025-06-21 02:35:56 UTC 1.122.16.112

--- part2/public/javascripts/page.js
+++ part2/public/javascripts/page.js
@@ -211,28 +211,7 @@ fetch ('/api/users/login',{
 .catch(err => alert('Error'));
 }
 
-///////////////////////////////////altered above////////////////////
 
-
-    // Create AJAX Request
-    var xmlhttp = new XMLHttpRequest();
-
-    // Define function to run on response
-    xmlhttp.onreadystatechange = function() {
-        if (this.readyState == 4 && this.status == 200) {
-            alert("Welcome "+this.responseText);
-        } else if (this.readyState == 4 && this.status >= 400) {
-            alert("Login failed");
-        }
-    };
-
-    // Open connection to server & send the post data using a POST request
-    // We will cover POST requests in more detail in week 8
-    xmlhttp.open("POST", "/users/login", true);
-    xmlhttp.setRequestHeader("Content-type", "application/json");
-    xmlhttp.send(JSON.stringify(user));
-
-}
 
 function logout(){
 
