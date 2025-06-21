245e33db76d0a3cfb7ac14d0acd0f124aadbfe32
apostolV450 <a1826273@student.adelaide.edu.au>
2025-06-21 05:22:12 +0000

    a

--- part2/routes/walkRoutes.js
+++ part2/routes/walkRoutes.js
@@ -60,7 +60,7 @@ router.post('/:id/apply', async (req, res) => {
 });
 //////////////altered below////////////////////////
 
-router.get('/dogs,async(req,res) =>{
+router.get('/dogs',async(req,res) =>{
   const user = req.session?.user;
   if(!user) return res.sendStatus(401);
   try{ const [dogs] = await db.query(
