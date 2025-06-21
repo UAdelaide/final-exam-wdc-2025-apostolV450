6ee53040337689d14130fb34fd36fc0192c0ff03
apostolV450 <a1826273@student.adelaide.edu.au>
2025-06-21 03:00:24 +0000

    [automatic commit] 2025-06-21 03:00:24 UTC 1.122.16.112

--- part2/public/javascripts/page.js
+++ part2/public/javascripts/page.js
@@ -178,7 +178,7 @@ function login(event){
  event.preventDefault();
 
 let user = {
-    username: document.getElementById('username').value,
+    email: document.getElementById('email').value,
     password: document.getElementById('password').value
 };
 
