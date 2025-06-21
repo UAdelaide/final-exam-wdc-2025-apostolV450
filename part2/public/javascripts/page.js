34840b59c1ee270d7e2e73efad02ba79fcddaf38
apostolV450 <a1826273@student.adelaide.edu.au>
2025-06-21 04:00:53 +0000

    [automatic commit] 2025-06-21 04:00:53 UTC 1.122.16.112

--- part2/public/javascripts/page.js
+++ part2/public/javascripts/page.js
@@ -221,7 +221,8 @@ fetch ('/api/users/login',{ // Send a POST request to the server
 
 function logout(){ // Function to handle logout
 // Send a POST request to the server to log out the user
-fetch('/api/users/logout', {
+fetch('/api/users/logout', {method: 'POST'})
+. {
     method:'POST', // Specify the method as POST
 });
 
